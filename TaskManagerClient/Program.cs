4bd29558855d26f9db36b89466f67ddda8b3ee2b
Fuipon <12385fnm@gmail.com>
2025-05-19 18:09:25 +0300

    1

--- TaskManagerClient/Program.cs
+++ TaskManagerClient/Program.cs
@@ -12,8 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ApiService>();
 
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 
 
